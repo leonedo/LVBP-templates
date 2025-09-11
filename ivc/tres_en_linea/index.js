8fe4a92fbb24ddbeb3095ea31388f0c601a55511
Leon Hurtado <leon@zype.com>
2025-09-11 13:59:34 -0300

    mas de 3 en linea

--- ivc/tres_en_linea/index.js
+++ ivc/tres_en_linea/index.js
@@ -40,7 +40,7 @@ const loadAnimation = (data, container) => {
     });
 }
 
-let anim = loadAnimation('animation.json', animContainer)
+let anim = loadAnimation(json_file, animContainer)
 let externalLoop;
 
 //add font-face from data.json  
