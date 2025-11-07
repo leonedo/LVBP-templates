0d3abb7592b64ff012659cafdb9167f1014a210c
Leonedo <leon@leonedo.com>
2025-11-07 14:23:51 -0300

    presentacion_pub

--- ivc/presentacion_pub/index.js
+++ ivc/presentacion_pub/index.js
@@ -40,7 +40,7 @@ const loadAnimation = (data, container) => {
     });
 }
 
-let anim = loadAnimation('data.json', animContainer)
+let anim = loadAnimation('animation.json', animContainer)
 let externalLoop;
 
 //add font-face from data.json  
